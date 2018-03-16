b6f3125a5b86a716b0281c2cd3e782efdc0a91fd
frogfather <j.a.campbell@icloud.com>
2018-03-16 11:58:52 +0000

    Added getters and setters for width, height and background colour

---
+++ Nonogram/Cell.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Nonogram
+{
+    public class Cell
+    {
+        public Cell()
+        {
+        }
+
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _width = value;
+                }
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _height = value;
+                }
+            }
+        }
+
+        public string BackgroundColour
+        {
+            get
+            {
+                return _backgroundColour;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    _backgroundColour = value;
+                }
+            }
+        }
+
+        //pattern should be an enum. 
+        //or should it be more abstract?
+
+        private int _width;
+        private int _height;
+        private string _backgroundColour; //check it is a colour?
+        private string _pattern; 
+    }
+}
