0db6b0ade7428812dae2fcf63702a573a7c780db
frogfather <j.a.campbell@icloud.com>
2018-03-25 16:29:40 +0100

    New class to hold a List of clue objects

--- Nonogram/Clues.cs
+++ Nonogram/Clues.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Nonogram
 {
     public class Clues
     {
+        //creates new clue objects and adds them to a list
         public Clues()
         {
+           
         }
+
+        private List<Clue> _clueList;
     }
 }
