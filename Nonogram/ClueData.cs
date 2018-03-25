1320caa236d4906fd1bf130bd02aecc5437421d6
frogfather <j.a.campbell@icloud.com>
2018-03-25 16:29:33 +0100

    New struct to allow clue data to be passed to Clues object

---
+++ Nonogram/ClueData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Nonogram
+{
+    public struct ClueData
+    {
+        public int value;
+        public string colour;
+
+        public ClueData(int clueValue, string clueColour)
+        {
+            value = clueValue;
+            colour = clueColour;
+        }
+    }
+}
---
+++ Nonogram/Clues.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Nonogram
+{
+    public class Clues
+    {
+        public Clues()
+        {
+        }
+    }
+}
