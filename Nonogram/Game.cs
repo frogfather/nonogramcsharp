ed2d793b91a92eaedbc4c57a1e8fb9853501c5a5
frogfather <j.a.campbell@icloud.com>
2018-03-26 11:59:01 +0100

    Add game class

--- Nonogram/Game.cs
+++ Nonogram/Game.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Nonogram
 {
     public class Game
     {
-        public Game()
+        //need to pass in info for grid (rows and columns)
+        //and info for rows (list of list of clueData)
+        //and info for columns (same as above)
+        public Game(int gameRows, int gameCols)
         {
+            _grid = new Grid(gameRows, gameCols);
+            _rows = new List<Clues>();
+            _columns = new List<Clues>();
         }
+        private Grid _grid;
+        private List<Clues> _rows;
+        private List<Clues> _columns;
     }
 }
