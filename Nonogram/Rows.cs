a50055462f284492b3cc98424c49d4eba592d31c
frogfather <j.a.campbell@icloud.com>
2018-03-26 11:59:09 +0100

    Add rows class

--- Nonogram/Rows.cs
+++ Nonogram/Rows.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Nonogram
 {
     public class Rows
     {
-        public Rows()
+        //we need to feed a List of Lists of ClueData into the constructor
+        public Rows(List<List<ClueData>> rowOptions);
         {
+            foreach (List<ClueData> item in rowOptions)
+            {
+                
+            }
         }
+
+        private List<Clues> rowList = new List<Clues>();
     }
 }
